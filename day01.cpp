8cb592f4d5206f878a0779666645fa44293bb7c8
nico <nico.campolongo@gmail.com>
2025-01-17 17:26:38 +0100

    running pre-commit hooks

--- day01.cpp
+++ day01.cpp
@@ -1,49 +1,47 @@
-#include <iostream>
 #include <fstream>
-#include <string>
+#include <iostream>
 #include <regex>
-#include <vector>
 #include <sstream>
-
+#include <string>
+#include <vector>
 
 int get_fuel(int mass) {
-    int total_fuel = 0;
-    while (mass > 0) {
-        int required_fuel = mass / 3 - 2;
-        total_fuel += std::max(required_fuel, 0);
-        mass = required_fuel;
-    }
-    return total_fuel;
+  int total_fuel = 0;
+  while (mass > 0) {
+    int required_fuel = mass / 3 - 2;
+    total_fuel += std::max(required_fuel, 0);
+    mass = required_fuel;
+  }
+  return total_fuel;
 }
 
-
 int main() {
-    std::ifstream file("inputs/d01");
-    if (!file.is_open()) {
-        std::cerr << "Unable to open file!" << std::endl;
-        return 1;
+  std::ifstream file("inputs/d01");
+  if (!file.is_open()) {
+    std::cerr << "Unable to open file!" << std::endl;
+    return 1;
+  }
+
+  std::string line;
+  std::regex re("\\d+");  // Regular expression to match integers
+
+  int total_fuel{0};
+  int part_2{0};
+  while (std::getline(file, line)) {
+    std::vector<int> line_nums;
+    std::sregex_iterator begin(line.begin(), line.end(), re), end;
+
+    for (auto it = begin; it != end; ++it) {
+      line_nums.push_back(std::stoi(it->str()));
     }
 
-    std::string line;
-    std::regex re("\\d+"); // Regular expression to match integers
+    total_fuel += line_nums[0] / 3 - 2;
+    part_2 += get_fuel(line_nums[0]);
+  }
+  file.close();
 
-    int total_fuel{0};
-    int part_2{0};
-    while (std::getline(file, line)) {
-        std::vector<int> line_nums;
-        std::sregex_iterator begin(line.begin(), line.end(), re), end;
+  std::cout << total_fuel << std::endl;
+  std::cout << part_2 << std::endl;
 
-        for (auto it = begin; it != end; ++it) {
-            line_nums.push_back(std::stoi(it->str()));
-        }
-
-        total_fuel += line_nums[0] / 3 - 2;
-        part_2 += get_fuel(line_nums[0]);
-    }
-    file.close();
-
-    std::cout << total_fuel << std::endl;
-    std::cout << part_2 << std::endl;
-
-    return 0;
+  return 0;
 }
--- day02.cpp
+++ day02.cpp
@@ -1,74 +1,73 @@
-#include <iostream>
 #include <fstream>
-#include <string>
+#include <iostream>
 #include <regex>
-#include <vector>
 #include <sstream>
-
+#include <string>
+#include <vector>
 
 int solve(std::vector<int> nums, int noun, int verb, bool debug = false) {
-    nums[1] = noun;
-    nums[2] = verb;
+  nums[1] = noun;
+  nums[2] = verb;
 
-    size_t idx = 0;
-    while (nums[idx] != 99) {
-        int idx1 = nums[idx + 1];
-        int idx2 = nums[idx + 2];
-        int idx3 = nums[idx + 3];
-        switch (nums[idx])
-        {
-        case 1:
-            nums[idx3] = nums[idx1] + nums[idx2];
-            break;
-        case 2:
-            nums[idx3] = nums[idx1] * nums[idx2];
-            break;
-        default:
-            throw std::runtime_error("Unsupported operation in nums at index: " + std::to_string(idx));;
-        }
-        idx += 4;
-        if (debug) {
-            for (auto n : nums) {
-                std::cout << n << ",";
-            }
-            std::cout << "\n";
-        }
+  size_t idx = 0;
+  while (nums[idx] != 99) {
+    int idx1 = nums[idx + 1];
+    int idx2 = nums[idx + 2];
+    int idx3 = nums[idx + 3];
+    switch (nums[idx]) {
+      case 1:
+        nums[idx3] = nums[idx1] + nums[idx2];
+        break;
+      case 2:
+        nums[idx3] = nums[idx1] * nums[idx2];
+        break;
+      default:
+        throw std::runtime_error("Unsupported operation in nums at index: " +
+                                 std::to_string(idx));
+        ;
     }
-    return nums[0];
+    idx += 4;
+    if (debug) {
+      for (auto n : nums) {
+        std::cout << n << ",";
+      }
+      std::cout << "\n";
+    }
+  }
+  return nums[0];
 }
 
-
 int main() {
-    std::ifstream file("inputs/d02");
-    if (!file.is_open()) {
-        std::cerr << "Unable to open file!" << std::endl;
-        return 1;
+  std::ifstream file("inputs/d02");
+  if (!file.is_open()) {
+    std::cerr << "Unable to open file!" << std::endl;
+    return 1;
+  }
+
+  std::string line;
+  std::regex re("\\d+");  // Regular expression to match integers
+
+  while (std::getline(file, line)) {
+    std::vector<int> line_nums;
+    std::sregex_iterator begin(line.begin(), line.end(), re), end;
+
+    for (auto it = begin; it != end; ++it) {
+      line_nums.push_back(std::stoi(it->str()));
     }
 
-    std::string line;
-    std::regex re("\\d+"); // Regular expression to match integers
+    // part 1
+    std::cout << solve(line_nums, 12, 2) << std::endl;
 
-    while (std::getline(file, line)) {
-        std::vector<int> line_nums;
-        std::sregex_iterator begin(line.begin(), line.end(), re), end;
-
-        for (auto it = begin; it != end; ++it) {
-            line_nums.push_back(std::stoi(it->str()));
+    // part 2
+    for (int noun = 0; noun < 100; ++noun) {
+      for (int verb = 0; verb < 100; ++verb) {
+        int ans = solve(line_nums, noun, verb);
+        if (ans == 19690720) {
+          std::cout << 100 * noun + verb << std::endl;
         }
-
-        // part 1
-        std::cout << solve(line_nums, 12, 2) << std::endl;
-
-        // part 2
-        for (int noun = 0; noun < 100; ++noun) {
-            for (int verb = 0; verb < 100; ++verb) {
-                    int ans = solve(line_nums, noun, verb);
-                    if (ans == 19690720) {
-                        std::cout << 100 * noun + verb << std::endl;
-                    }
-            }
-        }
+      }
     }
-    file.close();
-    return 0;
-} 
+  }
+  file.close();
+  return 0;
+}
